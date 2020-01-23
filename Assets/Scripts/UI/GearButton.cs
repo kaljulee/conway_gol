66030c32b124348f535bfc3e6ff652bc0a3170d3
Kalju Lee <kaljuvlee@gmail.com>
2020-01-23 18:06:04 -0500

    gear button and screen shape

---
+++ Assets/Scripts/UI/GearButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GearButton : MonoBehaviour
+{
+    private Image image;
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        string fourColorShade = FourColor.LIGHTEST;
+        image.color = new Color(
+            FourColor.gameboyColorsRGB[fourColorShade][0],
+            FourColor.gameboyColorsRGB[fourColorShade][1],
+            FourColor.gameboyColorsRGB[fourColorShade][2]
+        );
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
