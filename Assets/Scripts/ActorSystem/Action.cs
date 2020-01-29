ccf32eb66378a1a15fd7d502d274ff7fa45fe15e
Kalju Lee <kaljuvlee@gmail.com>
2020-01-28 21:44:26 -0500

    actor structure

---
+++ Assets/Scripts/ActorSystem/Action.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action<T>
+{
+
+    public static readonly string[] Types = { "PRESSURE_CHANGE", "DESTROY", "REPLACE" };
+
+    public string actionType { get; private set; }
+    public T payload { get; private set; }
+    public Vector2 address { get; private set; }
+
+    Action(string incomingType, T incomingPayload, Vector2 incomingAddress)
+    {
+        actionType = incomingType;
+        payload = incomingPayload;
+        address = incomingAddress;
+    }
+
+}
