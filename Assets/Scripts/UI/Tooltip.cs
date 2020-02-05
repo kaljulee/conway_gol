b27e09a07e305908327c3831077fc6a8240046b4
Kalju Lee <kaljuvlee@gmail.com>
2020-02-05 15:19:57 -0500

    tooltip prefab

---
+++ Assets/Scripts/UI/Tooltip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : Menu
+{
+    public string tooltipText = "tppooltipppppp";
+    private Text text;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        text = gameObject.GetComponentInChildren<Text>();
+        rectTransform = GetComponent<RectTransform>();
+
+        text.text = tooltipText;
+        Color newColor = image.color;
+        newColor.a = 1;
+        image.color = newColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
